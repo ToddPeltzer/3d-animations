e89321017c2ecd6bf542265fecc021798d9df771
Todd Peltzer <toddpeltzer14@gmail.com>
2023-06-20 08:20:48 -0600

    added router

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { HashRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+root.render(
+    <Router>
+      <App />
+    </Router>
+);
