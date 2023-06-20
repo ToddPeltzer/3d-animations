d77346dde954c236143fb9d43eb8be73404da250
Todd Peltzer <toddpeltzer14@gmail.com>
2023-06-20 09:44:32 -0600

    deleted routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from './Home';
 import Nav from './components/Nav';
+import Jumbotron from "./components/jumbotron";
+import SoundSection from "./components/SoundSection";
+import DisplaySection from "./components/DisplaySection";
+import WebgiViewer from "./components/WebgiViewer";
 
 function App() {
 
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Nav />
-        <Routes>
-          <Route path="/3d-animations" element={<Home />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Nav />
+      <Jumbotron />
+      <SoundSection />
+      <DisplaySection />
+      <WebgiViewer />
+    </div>
   );
 }
 
